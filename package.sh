f4786b6ed3778e8ac08d05f872f0977451cd6c01
jlqwer <jlqwer@126.com>
2022-06-22 17:03:51 +0800

    修改配置

--- package.sh
+++ package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git clone  https://github.com/jlqwer/OpenwrtApp package/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
